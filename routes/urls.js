24f93084fffcd8e3a93cd529b9eb36588b580da3
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-09 12:32:02 +0300

    Create route to decode a url

--- routes/urls.js
+++ routes/urls.js
@@ -1,19 +1,24 @@
 const router = require("express").Router();
 const shortid = require("shortid");
 const validateUrl = require("../utils/validateUrl");
-const url = require('url');
 
-const urls = [];
-// [{original, short}]
+const urls = []; // [{original, short}]
 
-// Short URL Generator
+// Excode url
 router.post("/encode", async (req, res) => {
-    if (!req.body.origUrl) return res.status(400).json({ message: "Url is required" });
+    // Validate req.body
+    if (!req.body.origUrl)
+        return res.status(400).json({ message: "\"origUrl\"Url is required" });
+
     const { origUrl } = req.body;
 
+    // generate short id
     const urlId = shortid.generate();
+
+    // Validate that url is valid
     if (validateUrl(origUrl)) {
         try {
+            // Check if url is already encooded
             let url = urls.find((u) => u.original === origUrl);
             if (url) {
                 res.json(url.short);
@@ -28,6 +33,27 @@ router.post("/encode", async (req, res) => {
         }
     } else {
         res.status(400).json({ message: "Invalid Original Url" });
+    }
+});
+
+router.get("/decode", async (req, res) => {
+    // Validate req.body
+    if (!req.body.short)
+        return res.status(400).json({ message: "\"short\" Url is required" });
+
+    const { short } = req.body;
+
+    try {
+        // Check if we have url in memory
+        let url = urls.find((u) => u.short === short);
+        if (url) {
+            res.json({ original: url.original });
+        } else {
+            res.status(400).json({ message: "URL does not exist" });
+        }
+    } catch (error) {
+        console.log(err);
+        res.status(500).json({ message: "Server Error" });
     }
 });
 
